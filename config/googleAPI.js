002d68ae16445b3dd8f0f1de75ba7d4c52253cfc
deathquin <deathquin@gmail.com>
2018-04-30 11:16:23 +0900

    2018-04-30
    - Server
    * 변동없음
    - Front

--- config/googleAPI.js
+++ config/googleAPI.js
@@ -4,8 +4,8 @@ module.exports = (function() {
       config : {
           ClientId: "38608143199-ere7tr8cmaklt5fa7e0kjst7mjkcg2ru.apps.googleusercontent.com",
           ClientSecretKey: "L415HRMfyROaddl9DWbtrGGm",
-          //redirectUrl: "http://ec2-13-59-246-87.us-east-2.compute.amazonaws.com:3000/auth/google/callback"
-          redirectUrl: "http://localhost:3000/auth/google/callback"
+          redirectUrl: "http://ec2-13-59-246-87.us-east-2.compute.amazonaws.com:3000/auth/google/callback"
+          //redirectUrl: "http://localhost:3000/auth/google/callback"
       }
   }
 })();
